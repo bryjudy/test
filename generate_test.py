17b7e00b59f1702f66701c18c03750e2413a7e1a
bryjudy <brycejudy11@gmail.com>
2025-02-07 21:55:06 -0800

    Completed Issue #25: test

--- generate_test.py
+++ generate_test.py
@@ -1,4 +1,19 @@
 from class_class import Class
+from student_class import Student
 
-class_instance = Class()
-class_instance.print_students()
+class1 = Class("Class 1")
+class2 = Class("Class 2")
+
+students = []
+for i in range(10):
+    student = Student(f"Student {i+1}")
+    students.append(student)
+
+for i in range(5):
+    class1.add_student(students[i])
+
+for i in range(5, 10):
+    class2.add_student(students[i])
+
+class1.print_students()
+class2.print_students()
