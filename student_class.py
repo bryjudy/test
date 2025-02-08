07dd935b109b29c704dfe2c62208fecb7ba5b2a1
bryjudy <brycejudy11@gmail.com>
2025-02-07 21:21:57 -0800

    Completed Issue #14: Classes and students

---
+++ student_class.py
@@ -0,0 +1,10 @@
+class Student:
+    def __init__(self, age, birthday):
+        self.age = age
+        self.birthday = birthday
+
+    def get_age(self):
+        return self.age
+
+    def get_birthday(self):
+        return self.birthday
